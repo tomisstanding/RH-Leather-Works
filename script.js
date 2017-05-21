490b9e269200ab04498808aa3355e43b10d16056
Thomas Standing <tomcoap@gmail.com>
2017-05-20 22:14:32 -0400

    add sticky nav

---
+++ script.js
@@ -0,0 +1,19 @@
+//Click the hamburger menu to see the animation
+//You can click the X to close or click anywhere outside the menu
+
+$(document).ready(function(){
+  $('.hamburger-shell').click(function(){
+    $('#menu').slideToggle(300);
+    $('.top').toggleClass('rotate');
+    $('.middle').toggleClass('rotate-back');
+    $('.menu-name').toggleClass('bump');
+    $('.bg-cover').toggleClass('reveal');
+  });
+  $('.bg-cover').click(function(){
+    $('#menu').slideToggle(300);
+    $('.top').toggleClass('rotate');
+    $('.middle').toggleClass('rotate-back');
+    $('.menu-name').toggleClass('bump');
+    $('.bg-cover').toggleClass('reveal');
+  })
+});
